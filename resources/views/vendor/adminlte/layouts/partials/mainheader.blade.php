94040f4b013eccf0cfa768632e5f6f21d0a60cd6
dhayllin <dhayllin@hotmail.com>
2018-09-22 17:43:31 -0300

    Rota /jovens/create aberta para guest

--- resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
@@ -2,4 +2,15 @@
 <header class="main-header">
     <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation"></nav>
+    <li>
+            <a href="{{ url('/logout') }}" class="btn"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                {{ trans('adminlte_lang::message.signout') }}
+            </a>
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+                 <input type="submit" value="logout" style="display: none;">
+            </form>
+        </li>
 </header>
